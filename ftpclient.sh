d9d5729f738b16230965fbcabde7a730386da575
Lefteris Mamatas <emamatas@uom.edu.gr>
2023-10-02 09:27:29 +0000

    updates

--- ftpclient.sh
+++ ftpclient.sh
@@ -8,16 +8,20 @@ else
     exit 1
 fi
 
+filename="1gb-file"
+
+mkdir ~/tmp 2> /dev/null
+cd ~/tmp
+rm $filename 2> /dev/null
+
 while true; do
-  mkdir ~/tmp 2> /dev/null
-  cd ~/tmp
-  lftp -e 'get 1gb-file;exit' $hostname:2121 -u mn-wifi,mn-wifi
-  #lftp -u mn-wifi,mn-wifi -e "get 512mb-file;exit" localhost:2121 | awk -F '[()]' '{print $(NF-1)}'
+  lftp -e "get $filename;exit" $hostname:2121 -u mn-wifi,mn-wifi
+  #lftp -u mn-wifi,mn-wifi -e "get $filename;exit" localhost:2121 | awk -F '[()]' '{print $(NF-1)}'
   # Check the exit code of the command
   if [ $? -ne 0 ]; then
     echo "lftp execution failed"
     exit 1  
   fi
-  rm ~/tmp/512mb-file
+  rm $filename
   sleep 1
 done
