589ab69cef9a2e31a724f3299fe4c6f826d61ec2
Lefteris Mamatas <emamatas@uom.edu.gr>
2023-10-02 09:24:05 +0000

    updates

--- ftpclient.sh
+++ ftpclient.sh
@@ -11,7 +11,7 @@ fi
 while true; do
   mkdir ~/tmp 2> /dev/null
   cd ~/tmp
-  lftp -e 'get 512mb-file;exit' $hostname:2121 -u mn-wifi,mn-wifi
+  lftp -e 'get 1gb-file;exit' $hostname:2121 -u mn-wifi,mn-wifi
   #lftp -u mn-wifi,mn-wifi -e "get 512mb-file;exit" localhost:2121 | awk -F '[()]' '{print $(NF-1)}'
   # Check the exit code of the command
   if [ $? -ne 0 ]; then
